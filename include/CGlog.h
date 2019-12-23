6999b067cee1a134dd41221023d18f70cefbc0ac
gaoshiqiang <gaoshiqiang1987@163.com>
2019-12-23 20:43:11 +0800

    add log level enum
    Signed-off-by: gaoshiqiang <gaoshiqiang1987@163.com>

--- include/CGlog.h
+++ include/CGlog.h
@@ -3,6 +3,14 @@
 #include <time.h>
 #include <sys/time.h>
 
+typedef enum log_level
+{
+	LogVerbose = 0;
+	LogDebug = 1;
+	LogInfo = 2;
+	LogCritical = 3;
+}LogLevel;
+
 class CGlog{
 public:
     static void log(const char* fmt, ...);
