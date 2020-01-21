83fbe74fd3ea59f023f2ea2453abccfc93498b56
gaoshiqiang <gaoshiqiang1987@163.com>
2020-01-21 11:31:31 +0800

    format and sync header

--- source/epoll_serv/CEpollfd.h
+++ source/epoll_serv/CEpollfd.h
@@ -1,14 +1,14 @@
 #include "common.h"
 
 class CEpollfd{
-public:
+ public:
     CEpollfd();
     CEpollfd(const CEpollfd& epollfd);
     ~CEpollfd();
     CEpollfd & operator =(const CEpollfd& epollfd);
 
     /* member function */
-public:
+ public:
     int create(const char* addr, const short port);
     int recv(char* msg, const int msg_length);
     int send(const char* msg, const int msg_length);
--- source/lib/CGlog.h
+++ source/lib/CGlog.h
@@ -3,8 +3,17 @@
 #include <time.h>
 #include <sys/time.h>
 
+typedef enum log_level
+{
+	kLogVerbose = 0;
+	kLogDebug = 1;
+	kLogInfo = 2;
+	kLogWarning = 3;
+	kLogCritical = 4;
+}LogLevel;
+
 class CGlog{
-public:
+ public:
     static void log(const char* fmt, ...);
     static void log(const char* hint, int line, const char* fmt, ...);
 };
